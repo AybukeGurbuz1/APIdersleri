7782585c692eeae3f2d6325693d727cc37edded3
aybüke <aybukeegrbzz@gmail.com>
2022-10-29 19:59:25 +0300

    29/10

---
+++ src/test/java/base_url/ReqresBaseUrl.java
@@ -0,0 +1,14 @@
+package base_url;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+
+public class ReqresBaseUrl {
+
+    protected RequestSpecification spec;
+    @Before
+    public void setup() {
+        spec=new RequestSpecBuilder().setBaseUri("https://reqres.in/api").build();
+    }
+}
